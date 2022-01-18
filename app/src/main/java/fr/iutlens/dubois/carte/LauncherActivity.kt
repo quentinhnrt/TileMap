e7281c821c50b34f4ead167976c89b2bb8050dbe
Balfoo <florent.hode@gmail.com>
2022-01-18 10:45:26 +0100

    Correction des versions des bibliothèques

---
+++ app/src/main/java/fr/iutlens/dubois/carte/LauncherActivity.kt
@@ -0,0 +1,27 @@
+package fr.iutlens.dubois.carte
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+
+    fun main(view: android.view.View) {
+        val intent=Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun Fruit(view: android.view.View) {
+        val intent=Intent(this, fruitcatching::class.java)
+        startActivity(intent)}
+
+    fun Crossy(view: android.view.View) {
+        val intent=Intent(this, Crossyroad::class.java)
+        startActivity(intent)
+    }
+
+}
