a0b917022414a3c475edec60b0a4ce90172edba2
Balfoo <florent.hode@gmail.com>
2022-01-26 15:30:27 +0100

    Correction des versions des bibliothèques

---
+++ app/src/main/java/fr/iutlens/dubois/carte/quiz.kt
@@ -0,0 +1,11 @@
+package fr.iutlens.dubois.carte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class quiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
