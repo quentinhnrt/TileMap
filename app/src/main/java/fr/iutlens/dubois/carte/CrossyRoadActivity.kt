5cf9f5893175551d1229fb76ba2ddbd11fcee5cb
rolpf <jiroxane@hotmail.fr>
2022-02-02 15:03:01 +0100

    changement du timer

--- app/src/main/java/fr/iutlens/dubois/carte/CrossyRoadActivity.kt
+++ app/src/main/java/fr/iutlens/dubois/carte/CrossyRoadActivity.kt
@@ -1,7 +1,6 @@
 package fr.iutlens.dubois.carte
 
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,17 +8,18 @@ import androidx.appcompat.app.AppCompatActivity
 import fr.iutlens.dubois.carte.sprite.BasicSprite
 import fr.iutlens.dubois.carte.sprite.TiledArea
 import fr.iutlens.dubois.carte.transform.FocusTransform
-import java.util.*
-import kotlin.concurrent.schedule
+import fr.iutlens.dubois.carte.utils.RefreshHandler
+import fr.iutlens.dubois.carte.utils.TimerAction
 import kotlin.math.abs
 
-class CrossyRoadActivity : AppCompatActivity() {
+class CrossyRoadActivity : AppCompatActivity(), TimerAction {
     private val theRoad by lazy { TiledArea(R.drawable.road, Decor(Decor.crossy)) }
     private val hero by lazy { BasicSprite(R.drawable.character64, theRoad, 6.5F, 1.5F) }
     private val gameView by lazy { findViewById<GameView>(R.id.gameViewRoad) }
     private val tab = Decor.crossy
     private val beginX = hero.x
     private val beginY = hero.y
+    private var timer : RefreshHandler?= null;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +27,8 @@ class CrossyRoadActivity : AppCompatActivity() {
         setContentView(R.layout.activity_crossy_road)
         // Chargement des feuilles de sprites
 
-
+    timer = RefreshHandler(this)
+        timer?.scheduleRefresh(500)
 
         gameView.apply {
             background = theRoad
@@ -37,31 +38,15 @@ class CrossyRoadActivity : AppCompatActivity() {
         gameView.onTouch = this::onTouchMap
         gameView.invalidate()
 
-        Timer("toY").schedule(0, 500) {
+            //        Timer("toY").schedule(0, 500) {
 
-            val y = hero.y
-            val x = hero.x
-
-            when(tab[y.toInt()][x.toInt()]){
-                '6','4','5','3' -> gameOver()
-                '7' -> cancel()
-            }
-
-            hero.y += 1f
-            gameView.invalidate()
-
-            when(tab[y.toInt()][x.toInt()]){
-                '6','4','5','3' -> gameOver()
-                '7' -> win()
-            }
-
-
-        }
     }
 
     private fun win() {
-        val intent= Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        timer = null
+        finish()
+//        val intent= Intent(this, MainActivity::class.java)
+//        startActivity(intent)
     }
 
     private fun onTouchMap(
@@ -82,16 +67,7 @@ class CrossyRoadActivity : AppCompatActivity() {
         hero.y += dy
         gameView.invalidate()
 
-        val y = hero.y
-        val x = hero.x
-
-        when(tab[y.toInt()][x.toInt()]){
-            '6','4','5','3' -> gameOver()
-        }
-
-
-
-
+        check(hero.x, hero.y)
 
 
 
@@ -101,5 +77,31 @@ class CrossyRoadActivity : AppCompatActivity() {
     private fun gameOver(){
         hero.x = beginX
         hero.y = beginY
+
+    }
+
+    override fun update() {
+        if (timer==null) return
+
+        timer?.scheduleRefresh(300)
+        val y = hero.y
+        val x = hero.x
+
+        check(x, y)
+
+        hero.y += 1f
+        gameView.invalidate()
+
+
+
+
+
+    }
+
+    private fun check(x: Float, y: Float) {
+        when (tab[y.toInt()][x.toInt()]) {
+            '6', '4', '5', '3' -> gameOver()
+            '7' -> win()
+        }
     }
 }
