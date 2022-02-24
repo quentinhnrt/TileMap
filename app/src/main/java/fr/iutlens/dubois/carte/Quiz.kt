c5ad9f9025fd60210e4ef40ed584e8453bf2f9e9
Antonin Arnault <anto59240@gmail.com>
2022-02-24 10:01:47 +0100

    Premier visuel page quizz

--- app/src/main/java/fr/iutlens/dubois/carte/Quiz.kt
+++ app/src/main/java/fr/iutlens/dubois/carte/Quiz.kt
@@ -3,7 +3,9 @@ package fr.iutlens.dubois.carte
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class quiz : AppCompatActivity() {
+class Quiz : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
