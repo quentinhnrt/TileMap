95b23efd8188ad873e8d6075f3fc4e89fa315606
EngelibertTom <82039753+EngelibertTom@users.noreply.github.com>
2022-02-24 12:01:16 +0100

    update fruit catching

--- app/src/main/java/fr/iutlens/dubois/carte/LauncherActivity.kt
+++ app/src/main/java/fr/iutlens/dubois/carte/LauncherActivity.kt
@@ -12,6 +12,7 @@ class LauncherActivity : AppCompatActivity() {
 
         SpriteSheet.register(R.drawable.road, 5, 4, this)
         SpriteSheet.register(R.drawable.character64, 1, 1, this)
+        SpriteSheet.register(R.drawable.fruit, 1, 1, this)
     }
 
     fun main(view: android.view.View) {
