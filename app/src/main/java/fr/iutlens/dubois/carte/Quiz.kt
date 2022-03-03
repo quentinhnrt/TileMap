4ec14b372e590711b37fe54ac320f21f991c6301
Antonin Arnault <anto59240@gmail.com>
2022-03-03 16:56:13 +0100

    Création tableau bonne réponse pour 1 seul Questionnaire + comptage du nb de bonnes réponses Création d'un radio group à la place du LinearLayout pour pouvoir ne selectionner qu'un seul radio button par question

--- app/src/main/java/fr/iutlens/dubois/carte/Quiz.kt
+++ app/src/main/java/fr/iutlens/dubois/carte/Quiz.kt
@@ -1,32 +1,34 @@
 package fr.iutlens.dubois.carte
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class Quiz : AppCompatActivity() {
 
-
-    val q = arrayOf(
+    val q1 = arrayOf(
         arrayOf(
             "Avec GET où se trouve les informations à envoyer au Serveur ?",
-            "1/ Dans l'entête", "2/ Dans l'url", "3/ Dans un cookie du site"
-        ),
-        arrayOf("On peut coder un site en :", "1/HTML", "2/Python", "3/PHP", "4/JavaScript"),
+            "1/ Dans l'entête", "2/ Dans l'url", "3/ Dans un cookie du site","4/ Sur le fichier READ ME"),
         arrayOf(
-            "Avec GET où se trouve les informations à envoyer au Serveur ?",
-            "1/ Dans l'entête", "2/ Dans l'url", "3/ Dans un cookie du site"
-        ),
-        arrayOf("On peut coder un site en :", "1/HTML", "2/Python", "3/PHP", "4/JavaScript"),
+            "On peut coder un site en :", "1/Latin", "2/MySQL", "3/PHP", "4/AI"),
         arrayOf(
-            "Avec GET où se trouve les informations à envoyer au Serveur ?",
-            "1/ Dans l'entête", "2/ Dans l'url", "3/ Dans un cookie du site"
-        )
+            "Pour créer des documents intéractifs ou d'impression on utilise :",
+            "1/ InDesign", "2/ Photoshop", "3/ Illustrator","Audition"),
+        arrayOf(
+            "Quel est le numéro de salle de l'Amphi ?", "1/ 00 F", "2/Il n'y en a pas", "3/ 05 F", "4/ 666 F"),
+        arrayOf(
+            "Est-ce que MMI c'est génial ?",
+            "1/ Oui", "2/ Non","Bof" ,"Drip")
     )
 
-    val id = arrayOf(R.id.textViewQuestion, R.id.radioButtonR1,R.id.radioButtonR2,R.id.radioButtonR3)
+    val r1 = arrayOf(2, 3, 1, 2, 1)
+
+    val id = arrayOf(R.id.textViewQuestion, R.id.RadioButton1,R.id.RadioButton2,R.id.RadioButton3,R.id.RadioButton4)
 
     private var nQuestion = 0
 
@@ -36,8 +38,18 @@ class Quiz : AppCompatActivity() {
         loadQ(nQuestion)
     }
 
+
+
+    private var note: Int = 0
+
     fun next(view: View) {
         //val question: TextView = findViewById(R.id.textViewQuestion)
+        val RadioButton = findViewById<RadioButton>(id[r1[nQuestion]])
+        if (RadioButton.isChecked) {
+            note++
+        }
+        Log.d("score", note.toString())
+
         nQuestion++
         loadQ(nQuestion)
 
@@ -49,8 +61,7 @@ class Quiz : AppCompatActivity() {
         var i: Int
 
         for (i in id.indices) {
-            textView[i].text = q[n][i]
-
+            textView[i].text = q1[n][i]
         }
     }
 
