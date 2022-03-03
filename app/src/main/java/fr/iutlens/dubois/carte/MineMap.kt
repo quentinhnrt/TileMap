8868660df54dd9ef257b2f17ec8c0f9cbd278574
lancelot.hayward <flamingfossa@gmail.com>
2022-03-03 15:23:48 +0100

    Merge branch 'master' of https://github.com/LancelotHayward/TileMap
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/vcs.xml
    Fixed neighbours bug

--- app/src/main/java/fr/iutlens/dubois/carte/MineMap.kt
+++ app/src/main/java/fr/iutlens/dubois/carte/MineMap.kt
@@ -13,18 +13,47 @@ class MineMap( override val sizeX: Int, override val sizeY: Int) : TileMap {
     fun validTile(x: Int,y: Int): Boolean {
         return x >= 0 && x < sizeX && y >= 0 && y < sizeY
     }
+    fun showDirection (x: Int, y: Int, direction: String) {
+        var counter = 1
+        var differenceX = x
+        var differenceY = y
+        var limit = 0
+        if (direction == "right"){
+            var limit = sizeX - x
+        }
+        else if (direction == "down"){
+            var limit = sizeY - y
+        }
+        while (counter > -1 && counter < limit) {
+            when (direction) {
+                "up" -> differenceY = 0-counter
+                "right" -> differenceX = counter
+                "down" -> differenceY = counter
+                "left" -> differenceX = 0-counter
+            }
+            if (demineurGrid[y+differenceY][x+differenceX] != 0)
+            {
+                counter = -1
+            }
+            else {
+                displayGrid[y+differenceY][x+differenceX] = 1
+                counter++
+            }
+        }
+    }
     fun showSpace(x: Int, y: Int) {
         var check_up = 1
         var check_down = 1
+        var check_left = 1
+        var check_right = 1
         while (check_up > -1 && y-check_up >= 0) {
             if (demineurGrid[y-check_up][x] != 0) {
                 check_up = -1
             }
             else {
-                displayGrid[y.toInt()-check_up][x.toInt()] = 1
+                displayGrid[y.toInt() - check_up][x.toInt()] = 1
                 check_up++
             }
-            var check_left = 0
         }
         while (check_down > -1 && y+check_down < sizeY) {
             if (demineurGrid[y+check_down][x] != 0) {
@@ -33,6 +62,24 @@ class MineMap( override val sizeX: Int, override val sizeY: Int) : TileMap {
             else {
                 displayGrid[y.toInt()+check_down][x.toInt()] = 1
                 check_down++
+            }
+        }
+        while (check_left > -1 && x-check_left >= 0) {
+            if (demineurGrid[y][x-check_left] != 0) {
+                check_left = -1
+            }
+            else {
+                displayGrid[y.toInt()][x.toInt()-check_left] = 1
+                check_left++
+            }
+        }
+        while (check_right > -1 && x+check_right < sizeX) {
+            if (demineurGrid[y][x+check_right] != 0) {
+                check_right = -1
+            }
+            else {
+                displayGrid[y.toInt()][x.toInt()+check_right] = 1
+                check_right++
             }
         }
     }
@@ -54,20 +101,20 @@ class MineMap( override val sizeX: Int, override val sizeY: Int) : TileMap {
                     tile_value = 4
                     blues--
                 }
-            }
-            if (tile_value != 0) {
-                demineurGrid[tile_y][tile_x] = tile_value
-                displayGrid[tile_y][tile_x] = tile_value
-                val neighbours = arrayOf(-1,0,1)
-                for (i in neighbours) {
-                    for (j in neighbours) {
-                        if (validTile(tile_x+j, tile_y+i)) {
-                            //displayGrid[tile_y+i][tile_x+j] = tile_value + 3
-                            if (i != j) {
-                                demineurGrid[tile_y + i][tile_x + j] = tile_value + 3
-                            }
-                            else if (i !=0) {
-                                demineurGrid[tile_y + i][tile_x + j] = tile_value + 3
+                if (tile_value != 0) {
+                    demineurGrid[tile_y][tile_x] = tile_value
+                    //displayGrid[tile_y][tile_x] = tile_value
+                    val neighbours = arrayOf(-1,0,1)
+                    for (i in neighbours) {
+                        for (j in neighbours) {
+                            if (validTile(tile_x+j, tile_y+i)) {
+                                //displayGrid[tile_y+i][tile_x+j] = tile_value + 3
+                                if (i != j) {
+                                    demineurGrid[tile_y + i][tile_x + j] = tile_value + 3
+                                }
+                                else if (i !=0) {
+                                    demineurGrid[tile_y + i][tile_x + j] = tile_value + 3
+                                }
                             }
                         }
                     }
